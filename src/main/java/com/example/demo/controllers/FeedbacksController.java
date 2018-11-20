ce3516a7cb38e3a91c6c631a1968d94c4cb30d0c
akux9 <akux9@allstate.com>
2018-11-20 17:28:32 +0530

    feedback

--- src/main/java/com/example/demo/controllers/FeedbacksController.java
+++ src/main/java/com/example/demo/controllers/FeedbacksController.java
@@ -4,10 +4,7 @@ import com.example.demo.entities.Feedbacks;
 import com.example.demo.model.FeedbackModel;
 import com.example.demo.services.FeedbacksService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/feedbacks")
@@ -16,6 +13,7 @@ public class FeedbacksController {
     @Autowired
     private FeedbacksService feedbacksService;
 
+    @CrossOrigin(origins = "http://localhost:3000")
     @RequestMapping(value = {"","/"}  , method = RequestMethod.POST)
     public Feedbacks approvalWorkFlow(@RequestBody FeedbackModel feedbackModel){
         return this.feedbacksService.saveFeedbacks(feedbackModel);
